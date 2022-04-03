611dc2a0153b6394b0976a8a69ff79a9ed04e686
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2022-04-03 15:43:48 +0200

    Create coin.py

---
+++ coin.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+""" 
+Spyder Editor
+This is a temporary script file.
+"""
+
+
+
+i = [*range(1,11)]
+print(i)
+# x = 2
+# for i in range(1,11):
+#     lista1 = [i]
+#     print(lista1)
+        
+# for y in range(i,11):
+        #     z = [i]
+        #     #print(z)
+        #     if y % x == 0:
+        #         #y = False
+        #         print(y)
+    
+
+
+# x = 2
+# for i in range(1,3):
+#     #print(i)
+#     if i % x == 0:
+#         for y in range(i,11):
+#             z = i
+#             if y % x == 0:
+#                 #y = False
+#                 print(y)
+    
+        
+#             #if y % x == 0:
+#                 #i+=1
+#                 #x+=1
+#                 #print(y)
+#                 #x += 1
+                
+
+
+
+
+
